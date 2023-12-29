7a9522dd559121bef7824dcf147054ed971d3b75
Hitesh Sharma <hitesh.sharma@aqlix.com>
2023-12-29 09:45:53 +0530

    added

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,31 +3,43 @@ function Books() {
     const [data, setData] = useState([]);
     useEffect(() => {
         try{
-            fetch('http://localhost/aqlix/wp-json/wp/v2/posts').
+
+            fetch('https://webroottech.com/wp-json/wp/v2/posts').
             then((response)=>response.json()).then((res)=>{
-               setData(res)
-               console.log(res[0])
+                setData(res)
+                console.log(res[0])
             })
-        }catch(err){
-            alert('error')
+        }catch(error){
+                        console.log('i am in erro')
+        }finally{
+
+            if (!data){
+              return <h1>... loading</h1>
+            }
         }
+        
     }, [])
-    
     return (
-        data.map((book)=>{
-            return(
-                <>
-                <div  class="card" style={{width: "18rem"}}>
+
+        <>
+        <div className="row">
+
+      {data.map((book)=>{
+          return(
+             <div className="col-md-4">
+                 <div  class="card" style={{width: "18rem"}}>
             <img src="..." class="card-img-top" alt="..." />
             <div class="card-body">
                 <h5  class="card-title" dangerouslySetInnerHTML={{__html: book.title.rendered }}/>
                 <p class="card-text" dangerouslySetInnerHTML={{__html: book.excerpt.rendered}}/>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-primary">Read more</a>
             </div>
         </div>
-                </>
+             </div>
             )
-        })
+        })}
+        </div>
+        </>
     )
 }
 
